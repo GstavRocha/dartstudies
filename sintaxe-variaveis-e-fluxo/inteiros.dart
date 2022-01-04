e92c87f04f6299f0f65504868294d65c474270e2
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-01-04 09:54:05 -0300

    Pasta de teste de variáveis

---
+++ sintaxe-variaveis-e-fluxo/inteiros.dart
@@ -0,0 +1,9 @@
+void main() {
+  int idade = 35;
+  int dany = 33;
+  int lucky = 8;
+
+  print("Minha idade é $idade");
+  print("Minha idade é $dany");
+  print("Minha idade é $lucky");
+}
---
+++ sintaxe-variaveis-e-fluxo/real.dart
@@ -0,0 +1,5 @@
+void main() {
+  // Na linguagem Dart existe o double para armazenar numeros reais.
+  double real = 1.00;
+  print(real);
+}
