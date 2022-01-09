29fee278755422248925f294b496c841f99100da
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-01-09 08:33:33 -0300

    Estudando Booleanos no dart

---
+++ sintaxe-variaveis-e-fluxo/.vscode/Condicionais/booleanosss.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Testando Variáveis Booleanas; Palavra Reserdava = bool
+
+  int idade = 36;
+  bool maior = idade >= 18;
+  // Ele guarda o resultado desse booleano. No caso, ele retorna
+  // True;
+
+  if (maior) {
+    print(" Você pode entrar");
+  } else {
+    print(" Você não pode entrar");
+  }
+}
