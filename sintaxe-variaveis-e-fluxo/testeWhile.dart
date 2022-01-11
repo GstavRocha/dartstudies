a982c6ec14039a3df02510c8a2578d632bf245c0
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-01-11 19:38:06 -0300

    Testando while

---
+++ sintaxe-variaveis-e-fluxo/testeWhile.dart
@@ -0,0 +1,11 @@
+void main() {
+  int conta = 0;
+  // while = enquanto. Até a condição deixe de ser verdadeira.
+
+  while (conta < 10) {
+    // != diferente
+    print(" o numero é $conta");
+    conta++;
+    // ++ equivale a variável somado mais um.
+  }
+}
