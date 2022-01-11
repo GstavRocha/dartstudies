9e5ab6b8d1e6fa3e7bd122fbf53c6143f62db8e8
GustavoHCpunk <gustavo@henrimichel.com.br>
2022-01-11 20:46:50 -0300

    Atualizando

--- Atividades/tabuada.dart
+++ Atividades/tabuada.dart
@@ -1,10 +1,10 @@
 void main() {
-  print(" Fazendo a tabuada de 9");
   // laço de repetição aninhado;
   for (int multiplicando = 0; multiplicando <= 10; multiplicando++) {
+    print("Esta é a tabuada de $multiplicando");
     for (int i = 0; i <= 10; i++) {
-      int resultado = multiplicando * i;
-      print("$multiplicando X $i = $resultado");
+      //int resultado = multiplicando * i;
+      print("$multiplicando X $i = ${multiplicando * i}");
     }
   }
 }
